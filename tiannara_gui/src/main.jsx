b4ff987db248e07bd12be798880a0f5fc422e5cf
Kimiti4 <karamos473@gmail.com>
2026-03-08 23:54:49 +0000

    test gui 2

--- tiannara_gui/src/main.jsx
+++ tiannara_gui/src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
-import "./index.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootEl = document.getElementById("root");
+
+if (!rootEl) {
+  throw new Error('Root element with id="root" not found');
+}
+
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
